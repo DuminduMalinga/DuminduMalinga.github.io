0b754d7f529b0f2cdef25c740c0d9b42c3e29ce6
DuminduMalinga <malingabandara24256@gmail.com>
2026-06-19 00:55:55 +0530

    Update Portfolio

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -15,7 +15,7 @@ export const personal = {
   linkedinHandle: "dumindu-malinga",
   email: "malingabandara24256@gmail.com",
   phone: "+94 77 328 6562",
-  cvUrl: "https://drive.google.com/file/d/1HragpfAMW9kMDW97rtIfwdz0ovp-KYOz/view?usp=sharing",
+  cvUrl: "https://drive.google.com/uc?export=download&id=1HragpfAMW9kMDW97rtIfwdz0ovp-KYOz",
 };
 
 export const roles = [
